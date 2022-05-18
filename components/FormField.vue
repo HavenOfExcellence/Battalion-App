f086db208baf371acef1f888996ab27f9dc107c9
czyan <czyang2002@gmail,com>
2022-05-18 14:29:39 +0800

    working basic validation

---
+++ components/FormField.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { Field, ErrorMessage } from "vee-validate";
+
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  name: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    default: "text",
+  },
+});
+
+const { label, name, type } = props;
+
+console.log(label, name, type);
+console.log("asd");
+
+function returnfalse() {
+  return false;
+}
+</script>
+
+<template>
+  <Field :name="name" :label="label" :type="type" :rules="returnfalse" />
+  <ErrorMessage :name="name" />
+</template>
--- components/LoginPage.vue
+++ components/LoginPage.vue
@@ -1,3 +1,16 @@
 <template>
-  <div class="w-96 h-96 bg-green-200"></div>
+  <div class="w-96 h-96 bg-green-200">
+    <Form class="flex-col flex" @submit="onSubmit">
+      <FormField name="name"></FormField>
+      <FormField name="email" type="email"></FormField>
+      <FormField name="password" type="password"></FormField>
+    </Form>
+  </div>
 </template>
+<script setup>
+import { Form } from "vee-validate";
+
+function onSubmit(values) {
+  console.log(values);
+}
+</script>
