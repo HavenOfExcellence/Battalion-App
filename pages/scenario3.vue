10170a0c5920ad25afe79efcd98be1440ef63132
chuahziyang <czyang2002@gmail.com>
2022-06-07 14:30:08 +0800

    asd

--- pages/scenario3.vue
+++ pages/scenario3.vue
@@ -1,16 +1,16 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <ScenarioPage :scenario="3" title="Tech Support Scams"
-    nextlink="/scenario4"
-  >
+  <ScenarioPage :scenario="3" title="Tech Support Scams" nextlink="/scenario4">
     <img src="assets/Scam3.png" />
     <h2>
-      Tech Support Scam is a common method used by scammers to deceive
-      unsuspecting individuals into divulging their personal information such as
-      their bank account details. This infographic provides information on how
-      this scam typically works, the signs individuals can look out for if they
-      suspect they are being scammed, and how individuals can protect themselves
-      against such scams.
+      Anyone who believes they have fallen prey to such scams should take these
+      immediate steps: Uninstall any software that you have installed at the
+      instructions of the scammers. Log off and turn off your computer to limit
+      any further activities that the scammers can execute. Report the incident
+      to your bank to halt further activities relating to your bank accounts.
+      Change your Internet banking credentials and remove any unauthorised
+      payees who may have been added to your bank accounts. Report the matter to
+      the police.
     </h2>
   </ScenarioPage>
 </template>
--- pages/scenario4.vue
+++ pages/scenario4.vue
@@ -7,11 +7,14 @@
   >
     <img src="assets/Scam4.png" />
     <h2>
-      After befriending an attractive person (who is usually foreign) online, he
-      or she tells a tale about falling into trouble or hard times. The scammer
-      persists with the story to gain their victim's trust and adoration, then
-      asks for money as proof of love. Once the money is transferred, the
-      scammer disappears. Watch out for:
+      The police advised members of the public to observe these crime prevention
+      measures: Do not click on dubious URL links provided in unsolicited text
+      messages. Banks do not send SMSes containing links Always verify the
+      authenticity of claims of problems with your bank account or cards issued
+      by the bank with the official bank website or sources Never disclose your
+      personal or Internet banking details and OTPs to anyone Report any
+      fraudulent transactions to your bank immediately "Bank officers will never
+      ask for your banking details or OTPs over the phone or by SMS," SPF said.
     </h2>
   </ScenarioPage>
 </template>
